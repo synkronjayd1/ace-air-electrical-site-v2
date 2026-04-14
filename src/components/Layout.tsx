27c05156808ed831278a317b69cc3420cdb6ead1
synkronjayd1 <info@synkron.com.au>
2026-04-15 07:13:38 +1000

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -53,7 +53,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   <img 
   src="/images/logo.jpg" 
   alt="logo" 
-  className="h-20 scale-150 origin-left w-auto"
+  className="h-16 w-auto"
 />
 </Link>
 
